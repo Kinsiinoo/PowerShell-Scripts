1bf8b2d8ba67a3237f8d50212189d51bfbdafdcf
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2022-11-28 20:30:48 +0100

    Add AD Fix_countryCode

---
+++ AD/Fix_countryCode.ps1
@@ -0,0 +1,19 @@
+﻿$userOUs = "OU=ExampleBranch,OU=Users,DC=example,DC=com"
+$corrCountryCode = 840
+$Users = $null
+$fixedUser = $null
+
+$userOUs | ForEach-Object { $Users += Get-ADUser -filter 'enabled -eq $true' -searchbase $_ -Properties countryCode }
+
+foreach ($User in $Users) {
+    if ($User.countryCode -eq $corrCountryCode) {
+        Write-Host "$($User.Name) - Correct countryCode!" -ForegroundColor Green
+    } else {
+        Write-Host "$($User.Name) - Wrong countryCode!" -ForegroundColor Red
+        $User | Set-ADUser -Replace @{countryCode="$($corrCountryCode)"}
+        $fixedUser++
+    }
+}
+
+Write-Host "Number of fixed users: " -NoNewline
+$fixedUser
