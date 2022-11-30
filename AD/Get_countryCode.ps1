f88f8df71d7552f1b7c9d2c42106bd0c0533531d
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2022-11-30 16:38:28 +0100

    Add AD Get_countryCode comments

--- AD/Get_countryCode.ps1
+++ AD/Get_countryCode.ps1
@@ -1,8 +1,13 @@
-﻿$userOUs = "OU=ExampleBranch,OU=Users,DC=example,DC=com"
+﻿### Variables
+# Example user OU path
+$userOUs = "OU=ExampleBranch,OU=Users,DC=example,DC=com"
 $Users = $null
 
+# $Users = All enabled user from example user OU with countryCode property, sort by Name
 $userOUs | ForEach-Object { $Users += Get-ADUser -filter 'enabled -eq $true' -searchbase $_ -Properties countryCode | Sort-Object -Property Name }
 
+# Check every user
 foreach ($User in $Users) {
+    # Output users + countryCode
     Write-Host "$($User.Name) - $($User.countryCode)" -ForegroundColor Cyan
 }
