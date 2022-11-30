c1e13dcdb21598efa8a525629b4cd6cbadeda5b2
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2022-11-30 16:29:13 +0100

    Add AD Get_countryCode

---
+++ AD/Get_countryCode.ps1
@@ -0,0 +1,8 @@
+﻿$userOUs = "OU=ExampleBranch,OU=Users,DC=example,DC=com"
+$Users = $null
+
+$userOUs | ForEach-Object { $Users += Get-ADUser -filter 'enabled -eq $true' -searchbase $_ -Properties countryCode | Sort-Object -Property Name }
+
+foreach ($User in $Users) {
+    Write-Host "$($User.Name) - $($User.countryCode)" -ForegroundColor Cyan
+}
